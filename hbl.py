48c32dd2bf3312ad75271c77bc7648b1a55c2a6c
affan10 <affan.muddassir10@gmail.com>
2018-12-05 15:35:09 +0500

    Update hbl.py

--- hbl.py
+++ hbl.py
@@ -1,15 +1,15 @@
 from BeautifulSoup import BeautifulSoup
 import urllib2
-#import sched, time
+import sched, time
 from datetime import datetime
 import ftplib
 
-#date = str(datetime.now())
-#date = date.split()[0]
-#myfile = open("HBL " + date + ".csv", "a")
+date = str(datetime.now())
+date = date.split()[0]
+myfile = open("HBL " + date + ".csv", "a")
 myfile = open("HBL.csv", "a")
 NoneType = type(None)
-#s = sched.scheduler(time.time, time.sleep)
+s = sched.scheduler(time.time, time.sleep)
 
 #print "Oil Price In $/barrel:"
 def bring_index():
@@ -35,21 +35,21 @@ def bring_index():
         print t.text
         myfile.write(corrected_time + "\n")
         myfile.write(t.text + "\n")
-        # session = ftplib.FTP('ftp.mystocks.pk', 'mystocks', 'wnyc(%C7o,b_')
-        # file = open('HBL 2018-04-09.csv', 'ab+')
-        # session.cwd("/public_html")
-        # session.storbinary('STOR HBL 2018-04-09.csv', file)  # send the file
-        # file.close()  # close file and FTP
-        # session.quit()
-        # print "Done"
-        # global value
-        # if t.text != value:
-        #     value = t.text
-        #     myfile.write(corrected_time + "\n")
-        #     myfile.write(t.text + "\n")
-        #     print "added"
-        # else:
-#     print "Not added"
+        session = ftplib.FTP('ftp.mystocks.pk', 'mystocks', 'wnyc(%C7o,b_')
+        file = open('HBL 2018-04-09.csv', 'ab+')
+        session.cwd("/public_html")
+        session.storbinary('STOR HBL 2018-04-09.csv', file)  # send the file
+        file.close()  # close file and FTP
+        session.quit()
+        print "Done"
+        global value
+        if t.text != value:
+            value = t.text
+            myfile.write(corrected_time + "\n")
+            myfile.write(t.text + "\n")
+            print "added"
+        else:
+    print "Not added"
 
 if __name__ == "__main__":
     while (1):
