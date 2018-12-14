fc8031ad0dcfd9cd14ba814b96c89ce2b5cbd52a
Affan Muddassir <affan.muddassir10@gmail.com>
2018-12-14 19:54:59 +0500

    Update tohadoop.py

--- Prediction/tohadoop.py
+++ Prediction/tohadoop.py
@@ -4,20 +4,20 @@ from datetime import datetime
 
 print datetime.now().time()
 
-#pydoop.hdfs.hdfs(host='default', port=0, user=None, groups=None)
+pydoop.hdfs.hdfs(host='default', port=0, user=None, groups=None)
 
-# hdfs.mkdir('NEWS ARTICLES')
-# # hdfs.put('/home/hduser1/PycharmProjects/Crawler/NEWS.csv', 'NEWS ARTICLES/NEWS.csv')
-#
-# var = hdfs.mkdir('ALL UBL DATA')
-# print var
-# hdfs.mkdir('ALL HBL DATA')
-# hdfs.mkdir('ALL OGDCL DATA')
-# hdfs.mkdir('ALL ENGRO DATA')
-# hdfs.mkdir('ALL PSO DATA')
-# hdfs.mkdir('MISC')
+hdfs.mkdir('NEWS ARTICLES')
+# hdfs.put('/home/hduser1/PycharmProjects/Crawler/NEWS.csv', 'NEWS ARTICLES/NEWS.csv')
 
-# hdfs.put('/home/hduser1/PycharmProjects/Crawler/Unwanted Stuff', 'MISC/Unwanted')
-#hdfs.put('/home/hduser1/PycharmProjects/Crawler/kse 100', 'HISTORICAL/')
+var = hdfs.mkdir('ALL UBL DATA')
+print var
+hdfs.mkdir('ALL HBL DATA')
+hdfs.mkdir('ALL OGDCL DATA')
+hdfs.mkdir('ALL ENGRO DATA')
+hdfs.mkdir('ALL PSO DATA')
+hdfs.mkdir('MISC')
 
-#hdfs.hdfs.delete("/user/hduser1/HISTORICAL", recursive=True)
+hdfs.put('/home/hduser1/PycharmProjects/Crawler/Unwanted Stuff', 'MISC/Unwanted')
+hdfs.put('/home/hduser1/PycharmProjects/Crawler/kse 100', 'HISTORICAL/')
+
+hdfs.hdfs.delete("/user/hduser1/HISTORICAL", recursive=True)
